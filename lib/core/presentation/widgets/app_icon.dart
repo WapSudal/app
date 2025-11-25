922101e6b94139ed265802a571d203f36b678cac
ArpaAP <arpa1705@gmail.com>
2025-11-26 03:10:14 +0900

    feat: 커스텀 SVG 아이콘셋 적용

---
+++ lib/core/presentation/widgets/app_icon.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:vector_graphics/vector_graphics.dart';
+import '../../../gen/assets.gen.dart';
+
+class AppIcon extends StatelessWidget {
+  final SvgGenImage icon;
+  final double size;
+  final Color? color;
+
+  const AppIcon(this.icon, {super.key, this.size = 24.0, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return VectorGraphic(
+      loader: AssetBytesLoader(icon.path),
+      width: size,
+      height: size,
+
+      // 색상 적용 방식
+      colorFilter: ColorFilter.mode(
+        color ?? IconTheme.of(context).color!,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+}
