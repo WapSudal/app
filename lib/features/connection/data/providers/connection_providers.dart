c4ff361cae4dc88c95a0932e9d8c4e752e38c736
ArpaAP <arpa1705@gmail.com>
2025-12-19 21:26:48 +0900

    refactor: connection 피처에서 usecase 레이어 제거

--- lib/features/connection/data/providers/connection_providers.dart
+++ lib/features/connection/data/providers/connection_providers.dart
@@ -4,34 +4,24 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../datasources/connection_local_datasource.dart';
 import '../repositories/connection_repository_impl.dart';
 import '../../domain/repositories/connection_repository.dart';
-import '../../domain/usecases/send_connection_request_usecase.dart';
-import '../../domain/usecases/get_pending_requests_usecase.dart';
-import '../../domain/usecases/accept_connection_usecase.dart';
-import '../../domain/usecases/reject_connection_usecase.dart';
-import '../../domain/usecases/get_my_connections_usecase.dart';
-import '../../domain/usecases/revoke_connection_usecase.dart';
+
 import '../../../../core/providers/shared_preferences_provider.dart';
 
 part 'connection_providers.g.dart';
 
-// ==================== DataSource ====================
-
-/// Connection LocalDataSource Provider (Singleton)
 @Riverpod(keepAlive: true)
 ConnectionLocalDataSource connectionLocalDataSource(Ref ref) {
   final prefs = ref.watch(sharedPreferencesProvider);
   return ConnectionLocalDataSource(prefs);
 }
 
-// ==================== Repository ====================
-
-/// Connection Repository Provider
 @riverpod
 ConnectionRepository connectionRepository(Ref ref) {
   final localDataSource = ref.watch(connectionLocalDataSourceProvider);
 
   return ConnectionRepositoryImpl(
     localDataSource: localDataSource,
+    // TODO: 현재 사용자 id를 전역 provider 에서 가져오도록 수정 필요
     getCurrentUserId: () {
       // Firebase Auth에서 현재 사용자 ID 가져오기
       final user = FirebaseAuth.instance.currentUser;
@@ -41,48 +31,4 @@ ConnectionRepository connectionRepository(Ref ref) {
       return user.uid;
     },
   );
-}
-
-// ==================== UseCases ====================
-
-/// Send Connection Request UseCase Provider
-@riverpod
-SendConnectionRequestUseCase sendConnectionRequestUseCase(Ref ref) {
-  final repository = ref.watch(connectionRepositoryProvider);
-  return SendConnectionRequestUseCase(repository);
-}
-
-/// Get Pending Requests UseCase Provider
-@riverpod
-GetPendingRequestsUseCase getPendingRequestsUseCase(Ref ref) {
-  final repository = ref.watch(connectionRepositoryProvider);
-  return GetPendingRequestsUseCase(repository);
-}
-
-/// Accept Connection UseCase Provider
-@riverpod
-AcceptConnectionUseCase acceptConnectionUseCase(Ref ref) {
-  final repository = ref.watch(connectionRepositoryProvider);
-  return AcceptConnectionUseCase(repository);
-}
-
-/// Reject Connection UseCase Provider
-@riverpod
-RejectConnectionUseCase rejectConnectionUseCase(Ref ref) {
-  final repository = ref.watch(connectionRepositoryProvider);
-  return RejectConnectionUseCase(repository);
-}
-
-/// Get My Connections UseCase Provider
-@riverpod
-GetMyConnectionsUseCase getMyConnectionsUseCase(Ref ref) {
-  final repository = ref.watch(connectionRepositoryProvider);
-  return GetMyConnectionsUseCase(repository);
-}
-
-/// Revoke Connection UseCase Provider
-@riverpod
-RevokeConnectionUseCase revokeConnectionUseCase(Ref ref) {
-  final repository = ref.watch(connectionRepositoryProvider);
-  return RevokeConnectionUseCase(repository);
 }
